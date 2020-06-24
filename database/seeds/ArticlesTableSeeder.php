14e3a6e7c2509f094771fe53ed414c08016af5c9
jangtroubleta <jhh9507@gmail.com>
2020-06-25 08:58:27 +0900

    모바일 서버 피드백

--- database/seeds/ArticlesTableSeeder.php
+++ database/seeds/ArticlesTableSeeder.php
@@ -11,10 +11,13 @@ class ArticlesTableSeeder extends Seeder
      */
     public function run()
     {
-        // App\Article::create([
-        //     'title' => 'article_title_1',
-        //     'content' => 'article_content_1',
-        // ]);
+        App\Article::create([
+            'title' => '조언 부탁드립니다.',
+            'content' => '제가 BTS 작은 것들을 위한 시 연습중인데요 하이라이트 부분에서 포인트를 못잡고 있습니다. 혹시 팁 같은거 알려주실수 있나요?',
+            'answer' => '스탭에 집중하기보단 상체 동작에 포커싱하면 자연스러운 동작이 나오게 된답니다. 도움이 되셨으면 좋겠네요.',
+            'user_id' => 2,
+            'expert_id' => 1,
+        ]);
         // App\Article::create([
         //     'title' => 'article_title_2',
         //     'content' => 'article_content_2',
--- public/mobile/services/FeedbackList.php
+++ public/mobile/services/FeedbackList.php
@@ -3,6 +3,13 @@ require_once 'ConnectDate.php';
 
 $obj = new ConnectDate();
 
+$expert_id = $_GET["expert_id"];
+
+$query = "select articles.id, users.name, articles.title, articles.created_at, articles.answer
+from articles
+join users ON articles.user_id = users.id
+where articles.expert_id = " . $expert_id;
+
 $con = mysqli_connect($obj->ip, $obj->id, $obj->password, $obj->database);
 
 if (mysqli_connect_errno($con)) {
@@ -11,14 +18,15 @@ if (mysqli_connect_errno($con)) {
  
 mysqli_set_charset($con, "utf8");
 
-$res = mysqli_query($con, "select id,userid,name,class,expert_id from users");
+$res = mysqli_query($con, $query);
+
 
 while($row = mysqli_fetch_array($res)) {
-    $result['id'] = $row['id'];
-    $result['userid'] = $row["userid"];
+    $result['id'] = $row["id"];
     $result['name'] = $row["name"];
-    $result['port'] = $row["class"];
-    $result['expert_id'] = $row["expert_id"];
+    $result['title'] = $row["title"];
+    $result['date'] = $row["created_at"];
+    $result['answer'] = $row["answer"];
     $arr["result"][] = $result;
 }
 
