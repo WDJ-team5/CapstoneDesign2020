3a2bce2fa958a9f8036b02c538356b49f7d5293d
jangtroubleta <jhh9507@gmail.com>
2020-08-12 13:25:50 +0900

    mobile update

--- database/seeds/LecturesTableSeeder.php
+++ database/seeds/LecturesTableSeeder.php
@@ -23,7 +23,7 @@ class LecturesTableSeeder extends Seeder
 
         App\Lecture::create([
             'title' => 'TWICE TTで学ぶ講義。',
-            'content' => '트와이스 TT로 배우는 아이돌 댄스 기초강의입니다.',
+            'content' => 'TWICE TTで学ぶアイドルダンスの基礎講義です。',
             'video' => 'lecture_sample02',
             'image' => 'LectureImg/21.jpg',
             'genre_id' => 6,
@@ -32,7 +32,7 @@ class LecturesTableSeeder extends Seeder
 
         App\Lecture::create([
             'title' => 'ワクキング入門者のためのワクキングダンスステップ基礎',
-            'content' => '왁킹댄스 스텝기초강의입니다.',
+            'content' => 'ワクキングダンスのステップ基礎講義です。',
             'video' => '나중에 넣어',
             'image' => 'LectureImg/19.jpg',
             'genre_id' => 5,
@@ -41,7 +41,7 @@ class LecturesTableSeeder extends Seeder
 
         App\Lecture::create([
             'title' => 'ダンサーMENSYと一緒に学ぶガールズヒップホップ高級',
-            'content' => '걸스힙합 고급강의입니다.',
+            'content' => 'ガールズヒップホップの高級講義です。',
             'video' => '나중에 넣어',
             'image' => 'LectureImg/09.jpg',
             'genre_id' => 4,
@@ -50,7 +50,7 @@ class LecturesTableSeeder extends Seeder
 
         App\Lecture::create([
             'title' => 'EXO LOVESHOT振り付け講義',
-            'content' => 'EXO LOVESHOT 안무강의입니다.',
+            'content' => 'EXO LOVE SHOTの振り付け講義です。',
             'video' => '나중에 넣어',
             'video' => 'lecture_sample02',
             'image' => 'LectureImg/13.jpg',
@@ -60,7 +60,7 @@ class LecturesTableSeeder extends Seeder
 
         App\Lecture::create([
             'title' => 'TWICE FANCYを踊りながら学ぶ基本講義',
-            'content' => '아이돌 댄스 기초강의입니다.',
+            'content' => 'アイドルダンスの基礎講義です。',
             'video' => 'lecture_sample01',
             'image' => 'LectureImg/11.jpg',
             'genre_id' => 6,
@@ -70,7 +70,7 @@ class LecturesTableSeeder extends Seeder
 
         App\Lecture::create([
             'title' => 'BTS IDOLダンス基礎講義',
-            'content' => '아이돌 댄스 기초강의입니다.',
+            'content' => 'アイドルダンスの基礎講義です。',
             'video' => 'lecture_sample01',
             'image' => 'LectureImg/01.jpg',
             'genre_id' => 1,
@@ -79,7 +79,7 @@ class LecturesTableSeeder extends Seeder
 
         App\Lecture::create([
             'title' => 'ヒップホップステップ中級',
-            'content' => '힙합스텝 중급강의입니다.',
+            'content' => 'ヒップホップステップ中級講義です。',
             'video' => '나중에 넣어',
             'image' => 'LectureImg/02.jpg',
             'genre_id' => 4,
@@ -88,7 +88,7 @@ class LecturesTableSeeder extends Seeder
 
         App\Lecture::create([
             'title' => 'ビーボーイング風車回し',
-            'content' => '비보잉 풍차돌리기 강의입니다.',
+            'content' => 'ビーボーイング風車回し講義です。',
             'video' => '나중에 넣어',
             'image' => 'LectureImg/03.jpg',
             'genre_id' => 3,
@@ -97,7 +97,7 @@ class LecturesTableSeeder extends Seeder
 
         App\Lecture::create([
             'title' => 'ワクキングダンスステップ高級',
-            'content' => '왁킹댄스 스텝고급강의입니다.',
+            'content' => 'ワクキングダンスのステップ高級講義です。',
             'video' => '나중에 넣어',
             'image' => 'LectureImg/04.jpg',
             'genre_id' => 5,
@@ -108,7 +108,7 @@ class LecturesTableSeeder extends Seeder
 
         App\Lecture::create([
             'title' => 'ポッピンヒョンジュンと一緒に学ぶポッピンダンス基礎ステップ講義',
-            'content' => '팝핀 댄스 기초강의입니다.',
+            'content' => 'ポッピンダンスの基礎講義です。',
             'video' => '나중에 넣어',
             'image' => 'LectureImg/07.jpg',
             'genre_id' => 2,
@@ -120,7 +120,7 @@ class LecturesTableSeeder extends Seeder
 
         App\Lecture::create([
             'title' => '男子アイドルダンス高級ステップ講義',
-            'content' => '남자아이돌 댄스 고급스텝강의입니다.',
+            'content' => '男子アイドルダンス高級ステップ講義です。',
             'video' => '나중에 넣어',
             'image' => 'LectureImg/22.jpg',
             'genre_id' => 1,
@@ -131,7 +131,7 @@ class LecturesTableSeeder extends Seeder
 
         App\Lecture::create([
             'title' => 'Red Velvetダンスで学ぶ中級講義',
-            'content' => '아이돌 댄스 중급강의입니다.',
+            'content' => 'アイドルダンス中級講義です。',
             'video' => 'lecture_sample01',
             'image' => 'LectureImg/12.jpg',
             'genre_id' => 6,
@@ -140,7 +140,7 @@ class LecturesTableSeeder extends Seeder
 
         App\Lecture::create([
             'title' => 'ポッピンダンス関節曲げ講義',
-            'content' => '팝핀 댄스 관절꺾기 중급강의입니다.',
+            'content' => 'ポッピンダンス関節曲げ中級講義です。',
             'video' => '나중에 넣어',
             'image' => 'LectureImg/14.jpg',
             'genre_id' => 2,
@@ -151,7 +151,7 @@ class LecturesTableSeeder extends Seeder
 
         App\Lecture::create([
             'title' => 'ビーボーイング基本器',
-            'content' => '비보잉 기초강의입니다.',
+            'content' => 'ビーボーイング基礎講義です。',
             'video' => '나중에 넣어',
             'image' => 'LectureImg/15.jpg',
             'genre_id' => 3,
@@ -165,7 +165,7 @@ class LecturesTableSeeder extends Seeder
 
         App\Lecture::create([
             'title' => 'ポッピンダンス高級講義',
-            'content' => '팝핀 댄스 고급강의입니다.',
+            'content' => 'ポッピンダンス高級講義です。',
             'video' => '나중에 넣어',
             'image' => 'LectureImg/16.jpg',
             'genre_id' => 2,
@@ -177,7 +177,7 @@ class LecturesTableSeeder extends Seeder
 
         App\Lecture::create([
             'title' => 'ヒップホップステップ基礎',
-            'content' => '힙합스텝 기초강의입니다.',
+            'content' => 'ヒップホップステップの基礎講義です。',
             'video' => '나중에 넣어',
             'image' => 'LectureImg/17.jpg',
             'genre_id' => 4,
@@ -187,7 +187,7 @@ class LecturesTableSeeder extends Seeder
 
         App\Lecture::create([
             'title' => 'ビーボーイング腰回し',
-            'content' => '비보잉 허리돌리기강의입니다.',
+            'content' => 'ビーボーイング腰回し講義です。',
             'video' => '나중에 넣어',
             'image' => 'LectureImg/18.jpg',
             'genre_id' => 3,
@@ -201,7 +201,7 @@ class LecturesTableSeeder extends Seeder
 
         App\Lecture::create([
             'title' => 'ワクキングダンスステップ中級',
-            'content' => '왁킹댄스 스텝중급강의입니다.',
+            'content' => 'ワクキングダンスステップ中級講義です。',
             'video' => '나중에 넣어',
             'image' => 'LectureImg/20.jpg',
             'genre_id' => 5,
